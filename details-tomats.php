66926f1c90711c7417e9b1a3681aac84525827b3
Atar EL AZIZ <atar.el-aziz@edu.ece.fr>
2021-01-13 13:51:46 +0100

    Initial commit

---
+++ details-tomats.php
@@ -0,0 +1,47 @@
+<div class="modal fade details-2" id="details-2"  tableindex ="-2" role="dialog" aria-labelledby="details-2" aria-hidden="true">
+	<div class="modal-dialog modal-lg">
+		<div class="modal-content">
+			<div calss="modal-header">
+				<button class="close" type="button" data-dismiss="modal" aria-label="Close">
+				<span aria-hidden="true">&times;</span>
+				</button>
+
+				<h4 class="modal-title text-center">The Old Man And The Sea</h4>
+			</div>
+		<div calss="modal-body">
+			<div class="container-fluid">
+				<div class="row">
+					<div class="col-sm-6">
+						<div class="center-block">
+							<img src="images/book2.jpg" alt="alchimiste" class="details img-responsive">
+						</div>
+					</div>
+				<div class="col-sm-6">
+						<h4>Details</h4>
+						<p>Le Vieil Homme et la Mer est un court roman, ce qui explique qu'il est parfois considéré comme une nouvelle, écrit par l'écrivain américain Ernest Hemingway à Cuba en 1951 et publié en 1952. Il s'agit de la dernière œuvre de fiction majeure produite par Hemingway et publiée de son vivant.</p>
+						<hr/>
+						<p>Prix : 15.00€</p>
+						<form action="add_cart.php" method="post">
+							<div class="form-group">
+								<div class="col-xs-3">
+									<label for="quantity" id="quantity-label">Quantity : </label>
+									<input type="text" class="form-control" id="quantity" name="quantity">
+									
+								</div><br>
+								
+								
+							</div>
+							
+						</form>
+
+				</div>
+			</div>
+			</div>
+		</div>
+		<div class="modal-footer">
+			<button class="btn btn-default" data-dismiss='modal'>Close</button>
+			<button class="btn btn-warning" type="submit"><span class="glyphicon glyphicon-shopping-cart"></span>Add to cart</button>
+		</div>
+		</div>
+	</div>
+</div>
